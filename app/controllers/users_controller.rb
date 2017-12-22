a06060eb132b1986e2b526b0881c6278ad381059
Derek Morash <derek@ablesense.com>
2017-12-22 09:42:57 -0400

    working on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,48 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @title = "Users"
+    @users = User.all
+  end
+
+  def show
+    @title = @user.name
+    # debugger
+  end
+
   def new
     @title = "Sign up"
+    @user = User.new
+    # debugger
   end
+
+  def edit
+    @title = "Edit #{@user.name}"
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
